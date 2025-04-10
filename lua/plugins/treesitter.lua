a66bd3bdfeeeed26bb1a7f3ac144d9832b07e9b1
Josh LeBlanc <joshcapitals07@gmail.com>
2025-04-10 15:33:14 +0200

    treesitter annoying

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
 	config = function()
 	    require'nvim-treesitter.configs'.setup {
 
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
+		ensure_installed = { "lua" },
 
 		auto_install = true,
 
