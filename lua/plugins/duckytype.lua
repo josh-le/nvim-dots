7b54af583849a066c8ecf670187119f2ff222590
Josh LeBlanc <joshcapitals07@gmail.com>
2025-06-04 13:36:28 -0400

    added ducky type (on voyager :))

---
+++ lua/plugins/duckytype.lua
@@ -0,0 +1,21 @@
+return {
+    "kwakzalver/duckytype.nvim",
+    config = function()
+	require('duckytype').setup{
+	    -- expected = "python_keywords",
+	    number_of_words = 100,
+	    -- average_word_length = 5.69,
+
+	    -- local borders = { "none", "single", "double", "rounded", "solid", "shadow" }
+	    window_config = {
+		border = "rounded"
+	    },
+	}
+	vim.keymap.set("n", "<leader>dt", function()
+	    require('duckytype').Start("english_common")
+	end)
+	-- require('duckytype').Start("cpp_keywords")
+	-- require('duckytype').Start("python_keywords")
+	-- require('duckytype').Start("go_keywords")
+    end
+}
